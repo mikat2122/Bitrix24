950fb89c517b437cc92b4e1236ac8a37933363ee
mikat2122 <ikate.med@gmail.com>
2020-04-15 12:42:50 -0400

    Updated TaskTabPage class

--- src/test/java/com/nextbasecrm/pages/TaskTabPage.java
+++ src/test/java/com/nextbasecrm/pages/TaskTabPage.java
@@ -3,18 +3,21 @@ package com.nextbasecrm.pages;
 import com.nextbasecrm.base.TestBase;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
+import utility.Driver;
 
 public class TaskTabPage {
+    public TaskTabPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
     @FindBy(xpath = "//span/span[text()='Task']")
-    WebElement taskTab;
+    public WebElement taskTab;
 
     @FindBy(id = "tasks-task-priority-cb")
-    WebElement highPriorityCheckBox;
+    public WebElement highPriorityCheckBox;
 
-    @FindBy(id = "bx-b-uploadfile-task-form-lifefeed_task_form")
-    WebElement uploadFileIcon;
 
 
 
