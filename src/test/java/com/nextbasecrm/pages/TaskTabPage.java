f41eebf05b222b0ffc2bc1790b13bd21507c5992
mikat2122 <ikate.med@gmail.com>
2020-04-15 12:33:22 -0400

    add changes to TaskTabPage

--- src/test/java/com/nextbasecrm/pages/TaskTabPage.java
+++ src/test/java/com/nextbasecrm/pages/TaskTabPage.java
@@ -1,4 +1,21 @@
 package com.nextbasecrm.pages;
 
+import com.nextbasecrm.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.BeforeMethod;
+
 public class TaskTabPage {
+
+    @FindBy(xpath = "//span/span[text()='Task']")
+    WebElement taskTab;
+
+    @FindBy(id = "tasks-task-priority-cb")
+    WebElement highPriorityCheckBox;
+
+    @FindBy(id = "bx-b-uploadfile-task-form-lifefeed_task_form")
+    WebElement uploadFileIcon;
+
+
+
 }
