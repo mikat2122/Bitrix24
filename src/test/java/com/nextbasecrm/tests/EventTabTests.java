30e3f70a6843e0360860a6676c6c3690fe0d1712
Yourname <YourEmail>
2020-04-15 22:48:43 -0400

    undated xpath for listOfLinks, added waits

--- src/test/java/com/nextbasecrm/tests/EventTabTests.java
+++ src/test/java/com/nextbasecrm/tests/EventTabTests.java
@@ -1,51 +1,52 @@
 package com.nextbasecrm.tests;
 
 import com.nextbasecrm.base.TestBase;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utility.BrowserUtils;
 import utility.ConfigurationReader;
+
 import static org.testng.Assert.assertEquals;
+
 import java.util.List;
+
 import static org.testng.Assert.assertTrue;
 
 public class EventTabTests extends TestBase {
 
     @BeforeMethod
-    public void setUpMethod(){
+    public void setUpMethod() {
         loginPage.login(ConfigurationReader.getProperty("help_desk_username"), ConfigurationReader.getProperty("help_desk_password"));
     }
-    @Test (description = "User should be able to attach link by clicking on the link icon",
+
+    @Test(description = "User should be able to attach link by clicking on the link icon",
             dataProvider = "links")
-    public void linkTest(String linkText, String linkUrl) throws InterruptedException {
+    public void linkTest(String linkText, String linkUrl) {
         eventTabPage.attachLink(linkText, linkUrl);
-        Thread.sleep(2000);
         wait.until(ExpectedConditions.textToBePresentInElement(eventTabPage.savedLinkWithText, linkText));
         driver.switchTo().defaultContent();
         eventTabPage.sendButton.click();
-        List<String> listOfLinks = BrowserUtils.getElementsText(eventTabPage.listOfLinksAndUploads);
-        wait.until(ExpectedConditions.visibilityOfAllElements(eventTabPage.listOfLinksAndUploads));
+        BrowserUtils.waitForPageToLoad(5);
+        //driver.navigate().refresh();
+        wait.until(ExpectedConditions.visibilityOfAllElements(eventTabPage.listOfLinks));
+        List<String> listOfLinks = BrowserUtils.getElementsText(eventTabPage.listOfLinks);
         assertTrue(listOfLinks.contains(linkText));
-
-        //Assert.assertTrue(eventTabPage.savedLinkWithText.getAttribute("innerHTML").equals(linkText));
     }
 
-    @DataProvider (name = "links")
-    public Object[][] getLinks(){
-        return new Object [][]
+    @DataProvider(name = "links")
+    public Object[][] getLinks() {
+        return new Object[][]
                 {
                         {"NY Times", "https://www.nytimes.com/"},
-                        {"Washington Post","https://www.washingtonpost.com/regional/"},
-                        {"Economist","https://www.economist.com/"}
+                        {"Washington Post", "https://www.washingtonpost.com/regional/"},
+                        {"Economist", "https://www.economist.com/"}
                 };
     }
 
     @Test
-    public void uploadFileIconTest(){
+    public void uploadFileIconTest() {
         eventTabPage.eventTab.click();
         eventTabPage.uploadFileIcon.click();
         assertTrue(eventTabPage.uploadFileMenu.isDisplayed());
